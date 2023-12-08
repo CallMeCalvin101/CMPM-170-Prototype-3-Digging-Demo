86907da67ec723ebdb594181e5561d838e4963db
CallMeCalvin101 <calvinkurniadjaja@gmail.com>
2023-12-07 22:15:45 -0800

    Implemented Mouse and Clickable Objects

--- prototype/src/main.ts
+++ prototype/src/main.ts
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const canvas = document.getElementById("game");
+const canvas = document.getElementById("game")!;
 const gameHeight = (canvas! as HTMLCanvasElement).height;
 const gameWidth = (canvas! as HTMLCanvasElement).width;
 const ctx = (canvas! as HTMLCanvasElement).getContext("2d")!;
@@ -20,6 +20,13 @@ const gameController = { curState: 0, curOrder: 0 };
 
 gameController.curState = states.startGame;
 
+const gameMouse = { x: 0, y: 0 };
+
+function updateMousePos(x: number, y: number) {
+  gameMouse.x = x;
+  gameMouse.y = y;
+}
+
 class Clickable {
   constructor(
     readonly xPos: number,
@@ -28,5 +35,42 @@ class Clickable {
     readonly size = CLICKABLE_SIZE
   ) {}
 
-  isMouseInside(): boolean {}
+  isMouseInside(): boolean {
+    return (
+      Math.sqrt(
+        Math.pow(gameMouse.x - this.xPos, 2) +
+          Math.pow(gameMouse.y - this.yPos, 2)
+      ) < this.size
+    );
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = "red";
+    ctx.arc(this.xPos, this.yPos, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+
+    ctx.fillStyle = "black";
+    ctx.font = `${this.size}px sans-serif`;
+    ctx.fillText(
+      `${this.order}`,
+      this.xPos - this.size / 4,
+      this.yPos + this.size / 4
+    );
+  }
 }
+
+canvas.addEventListener("mouseenter", (e) => {
+  updateMousePos(e.offsetX, e.offsetY);
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  updateMousePos(e.offsetX, e.offsetY);
+});
+
+canvas.addEventListener("mousedown", (e) => {
+  updateMousePos(e.offsetX, e.offsetY);
+});
+
+const testClickable = new Clickable(gameHeight / 2, gameWidth / 2, 1);
+testClickable.draw();
